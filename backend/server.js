bb7a65af3b0459848c96f4005df96456aba101cf
Mo Amir Khan <moamirkhan@Mos-MacBook-Air.local>
2025-07-03 16:20:24 +0530

    my changes

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3001',
+  origin: 'https://amfiaeducation.onrender.com',
   methods: ['POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type']
 }));
