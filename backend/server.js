49b3876c819e6678afec4dbe7b19da7348d5bfbb
Amir Khan <moamirkhanat@gmail.com>
2025-07-19 13:26:52 +0530

    my changes

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'www.amfiaeducation.com', 'https://www.amfiaeducation.com/'],
+  origin: ['http://localhost:3000', 'www.amfiaeducation.com', 'https://www.amfiaeducation.com'],
   // methods: ['POST', 'OPTIONS'],
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type']
