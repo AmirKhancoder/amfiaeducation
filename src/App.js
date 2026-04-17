5da935740fbcb5b8885241687e7bcfeee631120c
Amir Khan <moamirkhanat@gmail.com>
2026-04-17 11:42:18 +0530

    your message

--- src/App.js
+++ src/App.js
@@ -27,35 +27,7 @@ import SchoolExcellence from './components/SchoolExcellence';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        {/* Navbar placed here will appear on all routes */}
-        <Navbar />
-        
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/home' element={<Home/>} />
-          <Route path='/about' element={<About/>} />
-          <Route path='/services' element={<Services />} />
-          <Route path='/careers' element={<Careers />} />
-          <Route path='/contact' element={<ContactForm />} />
-          <Route path='/getTouch' element={<ContactForm />} />
-          <Route path='/courses' element={<Curriculum />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:slug" element={<BlogPost />} />
-          <Route path='/applyPath' element={<ApplyNow/>}/>
-          <Route path='/privacy' element={<PrivacyPolicy/>}/>
-          <Route path='/terms' element={<TermsAndCondition/>}/>
-          <Route path='/refund' element={<Refund/>}/>
-          <Route path='/shipping' element={<Shipping/>}/>
-          <Route path='/register-term' element={<RegistraionTerms/>}/>
-          <Route path='/register-re' element={<RegistrationOfSt/>}/>
-          <Route path='/knowmore' element={<ScholarshipProgram/>}/>
-          <Route path='/teacher-training-Det' element={<TeacherTraining/>}/>
-          <Route path='/testimonials' element={<SuccessStories/>}/>
-          <Route path='/afrozTeacher' element={<TeacherPortfolio/>}/>
-          <Route path='/school-excellence' element={<SchoolExcellence/>}/>
-        </Routes>
-      </BrowserRouter>
+      <h1>Under maintenance..... Please Wait for a while</h1>
     </div>
   );
 }
